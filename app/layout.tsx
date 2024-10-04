93d30df351b93ad2f3471a4c71a37fdc146b44db
Priyanshu Pandey <priyanshupandey@Priyanshus-MacBook-Air.local>
2024-10-04 20:32:16 +0530

    css setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import React from "react";
 import localFont from "next/font/local";
-import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
